16ead6b02cee4c3f9ec73d1f13694d5cef19de6a
9dogs <hellishbot@gmail.com>
2019-10-13 13:04:10 +0300

    Add typing to PlatformABC

--- group_songlink_bot/platforms.py
+++ group_songlink_bot/platforms.py
@@ -1,5 +1,7 @@
+"""Supported platforms."""
 import re
 from abc import ABC
+from typing import Pattern, Union
 
 #: Supported platforms
 PLATFORMS = {}
@@ -9,13 +11,13 @@ class PlatformABC(ABC):
     """Platform data holder."""
 
     # Platform's SongLink name
-    songlink_key = None
+    songlink_key: str
     # RegEx to find platform's URL in a message text
-    url_re = None
+    url_re: Union[str, Pattern]
     # Human readable name which will appear in bot message
-    name = None
+    name: str
     # Order of platform link in bot message
-    order = None
+    order: int
 
     def __init_subclass__(cls):
         """Compile regex and add platform to a platform registry."""
@@ -28,7 +30,7 @@ class DeezerPlatform(PlatformABC):
     """Deezer platform."""
 
     songlink_key = 'deezer'
-    url_re = r'https?://([a-zA-Z\d-]+\.)*deezer\.com/[\S]*'
+    url_re = r'https?://([a-zA-Z\d-]+\.)*deezer\.com/[^\s.]*'
     name = 'Deezer'
     order = 0
 
@@ -37,7 +39,7 @@ class GoogleMusicPlatform(PlatformABC):
     """Google Music platform."""
 
     songlink_key = 'google'
-    url_re = r'https?://([a-zA-Z\d-]+\.)*play\.google\.com/music/[\S]*'
+    url_re = r'https?://([a-zA-Z\d-]+\.)*play\.google\.com/music/[^\s.]*'
     name = 'Google Music'
     order = 1
 
@@ -46,6 +48,6 @@ class SoundCloudPlatform(PlatformABC):
     """SoundCloud platform."""
 
     songlink_key = 'soundcloud'
-    url_re = r'https?://([a-zA-Z\d-]+\.)*soundcloud\.com/[\S]*'
+    url_re = r'https?://([a-zA-Z\d-]+\.)*soundcloud\.com/[^\s.]*'
     name = 'SoundCloud'
     order = 2
